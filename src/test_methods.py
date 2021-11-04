da5854b305c944c424d50a595df7b26bc41e2482
jasmin-guven <jasmin.guven@hotmail.com>
2021-11-04 17:42:38 +0000

    split pdb data into files, started tidying older code

---
+++ src/test_methods.py
@@ -0,0 +1,12 @@
+import random
+import numpy as np
+import pandas as pd
+
+sample_size = 1000
+
+sample = np.arange(0,21,0.1)
+
+DF = pd.read_csv('../data/ids/id_file_0.txt', header = None)
+DF = DF.drop(columns=[0])
+DF = DF.transpose()
+print(DF)
